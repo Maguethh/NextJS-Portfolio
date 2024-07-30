a5be4bebd7015f8ce33e868992103c566d99baf0
Magueth magueth <magueth17@hotmail.com>
2024-07-30 16:45:12 +0200

    images fix

--- data/data.ts
+++ data/data.ts
@@ -35,12 +35,12 @@ export const slides = [
     index: 0,
     title: "My Hotel Project Platform",
     subtitle: "Professionnal Project",
-    thumbnail: "/images/mhp-thumbnail.png",
+    thumbnail: "/images/mhp-thumbnail.PNG",
     images: [
-      "/images/mhp1.png",
-      "/images/mhp2.png",
-      "/images/mhp3.png",
-      "images/mhp4.png",
+      "/images/mhp1.PNG",
+      "/images/mhp2.PNG",
+      "/images/mhp3.PNG",
+      "images/mhp4.PNG",
     ],
     textTitle: "Detailed Information",
     textParagraphs: [
